bf1aae901082dd60a90e835bfdd3fbff450594cf
Achmad Rizky <97281297+ARZzxz@users.noreply.github.com>
2023-06-13 20:42:54 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,5 +3,7 @@ public class Driver {
         int number = 5; // Ganti dengan angka yang ingin Anda hitung kuadratnya
         int square = NumberChecker.calculateSquare(number);
         System.out.println("Kuadrat dari " + number + " adalah " + square);
+        System.out.println("Kuadrat dari " + number + " adalah " + square);
+        System.out.println("Kuadrat dari " + number + " adalah " + square);
     }
 }
